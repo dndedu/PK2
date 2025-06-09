72b8e740a9c91317dd32533db5d148a653fe458a
Daniel Domanski <daniel.domanski@udo.edu>
2025-06-09 14:49:40 +0000

    P3_improved_20250909T1649

--- .vscode/C/P3/dictionary.c
+++ .vscode/C/P3/dictionary.c
@@ -2,43 +2,44 @@
 #include <stdlib.h>
 #define LENGTH_MAX 2
 
-typedef enum status{
-    FREE;
-    TAKEN; //OCCUPIED
-    DELETED;
+typedef enum status
+{
+    FREE,
+    TAKEN, // OCCUPIED
+    DELETED
 } status_t;
 
 typedef struct element
 {
     int value;
-    status_t s = FREE;
-};
+    status_t status;
+} element_t;
 
-static element dictionary[LENGTH_MAX];
+static element_t dictionary[LENGTH_MAX];
 static int currentLength = 0;
 
 int hash(int v, int multiplier)
 {
-    return (v + multiplier) % LENGTH_MAX; //linear probing
+    return (v + multiplier) % LENGTH_MAX; // linear probing
 }
 
 int insert(int v)
 {
     if (currentLength < LENGTH_MAX)
     {
-        element neu;
-        neu.value = v;
-        // neu.status_t = FREE;
-
         int multiplier = 1;
         int pos = hash(v, multiplier);
-        while (!(dictionary[pos].status_t == TAKEN))
+        while (dictionary[pos].status == TAKEN)
         {
             multiplier++;
             pos = hash(v, multiplier);
         }
+
+        element_t neu;
+        neu.value = v;
+        neu.status = TAKEN;
+
         dictionary[pos] = neu;
-        dictionary[pos].status_t = TAKEN;
         currentLength++;
         return 1;
     }
@@ -47,38 +48,41 @@ int insert(int v)
 
 int delete(int v)
 {
-    int deleted = 0;
-
     int multiplier = 1;
     int pos = hash(v, multiplier);
 
-    while (dictionary[pos].value != v || dictionary[pos].status_t == DELETED)
+    // skipping entries
+    while (dictionary[pos].value != v || dictionary[pos].status == DELETED)
     {
         multiplier++;
         pos = hash(v, multiplier);
     }
+
     if (dictionary[pos].value == v)
     {
-        deleted = dictionary[pos].deleted = 1;
+        dictionary[pos].status = DELETED;
         currentLength--;
+        return 1;
     }
-    return deleted;
+
+    return 0;
 }
 
 int member(int v)
 {
     int multiplier = 1;
     int pos = hash(v, multiplier);
-    while (dictionary[pos].value != v || dictionary[pos].deleted)
-    // while (dictionary[pos].value != v && !dictionary[pos].deleted)
+    while (dictionary[pos].value != v || dictionary[pos].status == DELETED)
     {
         multiplier++;
         pos = hash(v, multiplier);
     }
+
     if (dictionary[pos].value == v)
     {
         return 1;
     }
+
     return 0;
 }
 
@@ -87,7 +91,7 @@ int main()
     int i;
     for (i = 0; i < LENGTH_MAX; i++)
     {
-        dictionary[i].deleted = 1;
+        dictionary[i].status == FREE;
     }
 
     printf("%d\n", insert(1));
