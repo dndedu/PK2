74a6b4fd3d14df4e7aa3048ddaced409943c5720
Daniel Domanski <daniel.domanski@udo.edu>
2025-06-09 19:43:16 +0000

    P4_20250609T2143

--- .vscode/C/P4/main.c
+++ .vscode/C/P4/main.c
@@ -1,138 +1,252 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Definition der Liste
-typedef struct Liste
-{
-    int daten;
-    struct Liste *next;
-} Liste;
+#define SIZE 5
 
-// Definition des Dictionarys
-typedef struct Dictionary
+// Struktur für die Listenelemente
+typedef struct ListNode
 {
-    Liste **listen;
-    int size;
+    int key;
+    struct ListNode *next;
+} ListNode;
+
+// Dictionary-Struktur
+typedef struct
+{
+    ListNode *table[SIZE];
 } Dictionary;
 
-// Funktion zum Erstellen eines neuen Dictionarys
-Dictionary *erstelle_dictionary(int size)
+// Globales Dictionary
+Dictionary dict;
+
+// Hash-Funktion
+int hash(int key)
 {
-    Dictionary *dict = malloc(sizeof(Dictionary));
-    dict->size = size;
-    dict->listen = malloc(size * sizeof(Liste *));
-    for (int i = 0; i < size; i++)
-    {
-        dict->listen[i] = NULL;
-    }
-    return dict;
+    return key % SIZE;
 }
 
-// Funktion zum Einfügen eines Elements in das Dictionary
-int insert(Dictionary *dict, int daten)
+// Initialisierung des Dictionaries
+void init_dictionary()
 {
-    int hash_index = daten % dict->size;
+    for (int i = 0; i < SIZE; i++)
+    {
+        dict.table[i] = NULL;
+    }
+}
 
-    Liste *neue_liste = malloc(sizeof(Liste));
-    neue_liste->daten = daten;
-    neue_liste->next = dict->listen[hash_index];
-    
-    dict->listen[hash_index] = neue_liste;
+// Insert-Operation (konstante Laufzeit)
+int insert(int key)
+{
+    int index = hash(key);
+
+    // Neuen Knoten erstellen
+    ListNode *new_node = (ListNode *)malloc(sizeof(ListNode));
+    if (new_node == NULL)
+    {
+        // Nicht genügend Speicherplatz
+        return 0;
+    }
+
+    // Neuen Knoten am Anfang der Liste einfügen
+    new_node->key = key;
+    new_node->next = dict.table[index];
+    dict.table[index] = new_node;
+
     return 1;
 }
 
-// Funktion zum Überprüfen, ob ein Element im Dictionary vorhanden ist
-int member(Dictionary *dict, int daten)
+// Member-Operation
+int member(int key)
 {
-    int hash_index = daten % dict->size;
-    Liste *aktuelle_liste = dict->listen[hash_index];
-    while (aktuelle_liste != NULL)
+    int index = hash(key);
+    ListNode *current = dict.table[index];
+
+    // Liste durchsuchen
+    while (current != NULL)
     {
-        if (aktuelle_liste->daten == daten)
+        if (current->key == key)
         {
             return 1;
         }
-        aktuelle_liste = aktuelle_liste->next;
+        current = current->next;
     }
+
     return 0;
 }
 
-// Funktion zum Löschen eines Elements aus dem Dictionary
-int delete(Dictionary *dict, int daten)
+// Delete-Operation
+int delete(int key)
 {
-    int hash_index = daten % dict->size;
-    Liste *vorherige_liste = NULL;
-    Liste *aktuelle_liste = dict->listen[hash_index];
-    while (aktuelle_liste != NULL)
+    int index = hash(key);
+    ListNode *current = dict.table[index];
+    ListNode *prev = NULL;
+
+    // Liste durchsuchen
+    while (current != NULL)
     {
-        if (aktuelle_liste->daten == daten)
+        if (current->key == key)
         {
-            if (vorherige_liste == NULL)
+            // Element gefunden - löschen
+            if (prev == NULL)
             {
-                dict->listen[hash_index] = aktuelle_liste->next;
+                // Erstes Element in der Liste
+                dict.table[index] = current->next;
             }
             else
             {
-                vorherige_liste->next = aktuelle_liste->next;
+                // Element in der Mitte oder am Ende
+                prev->next = current->next;
             }
-            free(aktuelle_liste);
+            free(current);
             return 1;
         }
-        vorherige_liste = aktuelle_liste;
-        aktuelle_liste = aktuelle_liste->next;
+        prev = current;
+        current = current->next;
     }
+
     return 0;
+}
+
+// Hilfsfunktion zur Ausgabe des Dictionary-Inhalts
+void print_dictionary()
+{
+    printf("Dictionary contents:\n");
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf("Bucket %d: ", i);
+        ListNode *current = dict.table[i];
+        while (current != NULL)
+        {
+            printf("%d -> ", current->key);
+            current = current->next;
+        }
+        printf("NULL\n");
+    }
+    printf("\n");
+}
+
+// Test-Funktion
+void run_test()
+{
+    printf("=== Test-Sequenz ===\n");
+    init_dictionary();
+
+    int i;
+
+    // Insert 1 bis 2*SIZE
+    printf("Insert 1 bis %d: ", 2 * SIZE);
+    for (i = 1; i <= 2 * SIZE; i++)
+    {
+        printf("%d", insert(i));
+    }
+    printf("\n");
+
+    // Member 1 bis SIZE
+    printf("Member 1 bis %d: ", SIZE);
+    for (i = 1; i <= SIZE; i++)
+    {
+        printf("%d", member(i));
+    }
+    printf("\n");
+
+    // Delete SIZE+1 bis 2*SIZE
+    printf("Delete %d bis %d: ", SIZE + 1, 2 * SIZE);
+    for (i = SIZE + 1; i <= 2 * SIZE; i++)
+    {
+        printf("%d", delete(i));
+    }
+    printf("\n");
+
+    // Member 1 bis 2*SIZE
+    printf("Member 1 bis %d: ", 2 * SIZE);
+    for (i = 1; i <= 2 * SIZE; i++)
+    {
+        printf("%d", member(i));
+    }
+    printf("\n\n");
+}
+
+// Hauptprogramm mit Kommandozeilen-Argumenten
+void run_interactive(int argc, char *argv[])
+{
+    printf("=== Interaktives Programm ===\n");
+    init_dictionary();
+
+    // Kommandozeilen-Argumente in Dictionary einfügen
+    for (int i = 1; i < argc; i++)
+    {
+        int num = atoi(argv[i]);
+        if (insert(num))
+        {
+            printf("Zahl %d eingefügt.\n", num);
+        }
+        else
+        {
+            printf("Fehler beim Einfügen von %d.\n", num);
+        }
+    }
+
+    printf("\nDictionary nach Kommandozeilen-Eingabe:\n");
+    print_dictionary();
+
+    // Interaktive Eingabe
+    printf("Geben Sie Zahlen ein (Ende mit -1):\n");
+    int input;
+    while (1)
+    {
+        printf("Zahl eingeben: ");
+        if (scanf("%d", &input) != 1)
+        {
+            printf("Ungültige Eingabe!\n");
+            continue;
+        }
+
+        if (input == -1)
+        {
+            printf("Programm beendet.\n");
+            break;
+        }
+
+        if (member(input))
+        {
+            printf("Die Zahl %d ist im Dictionary enthalten.\n", input);
+        }
+        else
+        {
+            printf("Die Zahl %d ist NICHT im Dictionary enthalten.\n", input);
+        }
+    }
+}
+
+// Speicher freigeben
+void cleanup()
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        ListNode *current = dict.table[i];
+        while (current != NULL)
+        {
+            ListNode *temp = current;
+            current = current->next;
+            free(temp);
+        }
+        dict.table[i] = NULL;
+    }
 }
 
 int main(int argc, char *argv[])
 {
-    int size = 1;
-    Dictionary *dict = erstelle_dictionary(size);
+    printf("Hash Dictionary mit Chaining (SIZE = %d)\n", SIZE);
+    printf("Hash-Funktion: h(a) = a mod %d\n\n", SIZE);
 
-    for (int i = 1; i <= 2 * size; i++)
-    {
-        printf("%d", insert(dict, i));
-    }
-    for (int i = 1; i <= size; i++)
-    {
-        printf("%d", member(dict, i));
-    }
-    for (int i = size + 1; i <= 2 * size; i++)
-    {
-        printf("%d", delete(dict, i));
-    }
-    for (int i = 1; i <= 2 * size; i++)
-    {
-        printf("%d", member(dict, i));
-    }
-    printf("\n");
+    // Test-Sequenz ausführen
+    run_test();
 
-    // // Alle Zahlen aus der Kommandozeile in das Dictionary aufnehmen
-    // for (int i = 1; i < argc; i++)
-    // {
-    //     int daten = atoi(argv[i]);
-    //     insert(dict, daten);
-    // }
+    // Interaktives Programm
+    run_interactive(argc, argv);
 
-    // // Zahlen über die Tastatur einlesen und prüfen, ob sie im Dictionary vorhanden sind
-    // int daten;
-    // while (1)
-    // {
-    //     printf("Geben Sie eine Zahl ein (-1 zum Beenden): ");
-    //     scanf("%d", &daten);
-    //     if (daten == -1)
-    //     {
-    //         break;
-    //     }
-    //     if (member(dict, daten))
-    //     {
-    //         printf("Die Zahl %d ist im Dictionary vorhanden.\n", daten);
-    //     }
-    //     else
-    //     {
-    //         printf("Die Zahl %d ist nicht im Dictionary vorhanden.\n", daten);
-    //     }
-    // }
+    // Speicher freigeben
+    cleanup();
 
     return 0;
 }
